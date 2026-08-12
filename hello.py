d2d20e4baa876a8f4f1d4beedbce486c5245f080
Raja <-GitHub-email>
2026-08-12 09:47:52 +0000

    Update hello.py on feature branch

--- hello.py
+++ hello.py
@@ -2,3 +2,5 @@ print("Hello from Git and Python!")
 Hello from Git and Python!
 Learning Git with Kali!
 My first GitHub project!
+print("Learning branches in Git!")
+print("Learning branches in Git!")
